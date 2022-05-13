650ef1e5ae34a9ea7207e558ba11b474cf28fe21
Albert Pichler <albertpichler@gmx.net>
2022-05-13 16:04:13 +0200

    Implement a super weird way to force a fs img download

--- src/PreviewArea.js
+++ src/PreviewArea.js
@@ -5,14 +5,44 @@ export default function PreviewArea(props) {
     anchorTag.click();
   } */
 
+  // doing this just to force a download to fs from the browser feels utterly ridiculus
+  function stolenDLCodeBlockFromSO() {
+    fetch(props.imgAddress, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'image/png',
+      },
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Create blob link to download
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `custom.img`);
+
+        // Append to html link element page
+        document.body.appendChild(link);
+
+        // Start download
+        link.click();
+
+        // Clean up and remove the link
+        link.parentNode.removeChild(link);
+      })
+      .catch(() => {});
+  }
+
   return (
     <>
       <h2>Your meme</h2>
 
-      <button>
-        <a id="DL" href={props.imgAddress} download="test">
-          Download
-        </a>
+      <button
+        onClick={() => {
+          stolenDLCodeBlockFromSO();
+        }}
+      >
+        Download
       </button>
 
       <a href={props.imgAddress}>
